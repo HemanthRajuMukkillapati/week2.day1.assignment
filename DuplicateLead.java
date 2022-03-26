38b02b19e4ee000108f51b5ce8f72f1e7be97023
HemanthRajuMukkillapati <95237992+HemanthRajuMukkillapati@users.noreply.github.com>
2022-03-26 23:26:04 +0530

    Add files via upload

---
+++ DuplicateLead.java
@@ -0,0 +1,89 @@
+package week2.day1.assignment;
+
+
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.support.ui.Select;
+
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	public class DuplicateLead {
+
+		public static void main(String[] args) throws InterruptedException {
+			
+			
+			WebDriverManager.chromedriver().setup();
+					
+			ChromeDriver driver =new ChromeDriver(); 	
+			
+			//Launch URL "http://leaftaps.com/opentaps/control/login"
+			driver.get("http://leaftaps.com/opentaps");
+			driver.manage().window().maximize();		
+			
+			//Enter UserName and Password Using Id Locator
+			
+			driver.findElement(By.id("username")).sendKeys("DemoCSR");	
+			driver.findElement(By.id("password")).sendKeys("crmsfa");
+			
+			//Click on Login Button using Class Locator
+			
+			driver.findElement(By.className("decorativeSubmit")).click();
+			
+			//Click on CRM/SFA Link
+			 	
+			driver.findElement(By.linkText("CRM/SFA")).click();
+			
+			driver.findElement(By.linkText("Leads")).click();			//Click on Leads Button
+			
+			driver.findElement(By.linkText("Create Lead")).click();   	//Click on Create Lead 
+			
+			driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Hety's Org"); //Enter CompanyName Field Using id Locator and using as variable since in Duplicate page also same id.
+			
+			driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hemanth1");  //Enter FirstName Field Using id Locator
+			
+			driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Raju1");			//Enter LastName Field Using id Locator
+			driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("local");	//Enter FirstName(Local) Field Using id Locator																//createLeadForm_firstNameLocal			
+			
+			driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing"); //Enter Department Field Using any Locator of Your Choice
+			
+			driver.findElement(By.id("createLeadForm_description")).sendKeys("Selenium Assignment");//Enter Description Field Using any Locator of your choice 
+			driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("hemanthraju.m1991@gmail.com");//Enter your email in the E-mail address Field using the locator of your choice
+			
+			WebElement stateProvince = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));//Select State/Province as NewYork Using Visible Text
+			
+			Select spDD = new Select(stateProvince);
+			
+			spDD.selectByVisibleText("New York");//Select State/Province as NewYork Using Visible Text
+			
+		    driver.findElement(By.className("smallSubmit")).click(); 	//Click on Create Button
+			
+			String title=driver.getTitle();
+			System.out.println(title);
+			
+			driver.findElement(By.linkText("Duplicate Lead")).click(); //click on Duplicate
+			 
+			WebElement compnayName = driver.findElement(By.id("createLeadForm_companyName"));
+			compnayName.clear();
+			String updateComp="company updated to new";     //update companyname variable
+			String updateFName="UpdatedName";				//update First name variable
+			compnayName.sendKeys(updateComp);
+			WebElement firstName = driver.findElement(By.id("createLeadForm_firstName"));
+			firstName.clear();
+			firstName.sendKeys(updateFName);
+			driver.findElement(By.className("smallSubmit")).click();
+			
+			
+			
+			String titleupdated=driver.getTitle();// get title
+ 			
+			
+
+			System.out.println("The page after creating duplicate details title:  " + titleupdated);
+		
+			
+	
+		}
+
+	}
